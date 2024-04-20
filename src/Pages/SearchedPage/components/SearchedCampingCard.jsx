7d20eec450081d1fb24cf75857bd1b7161ddf885
Molkang <mitier26@gmail.com>
2024-04-20 22:27:26 +0900

    버그 픽스?

--- src/Pages/SearchedPage/components/SearchedCampingCard.jsx
+++ src/Pages/SearchedPage/components/SearchedCampingCard.jsx
@@ -1,24 +1,65 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
-import ListGroup from 'react-bootstrap/ListGroup'
-import { Badge } from 'react-bootstrap'
-import './SearchedCampingCard.css'
-import { useNavigate } from 'react-router-dom'
-import noImage from '../../../assets/noImage.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLink, faPhone, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Badge } from 'react-bootstrap';
+import './SearchedCampingCard.css';
+import { useNavigate } from 'react-router-dom';
+import noImage from '../../../assets/noImage.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLink, faPhone, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
-const SearchedCampingCard = ({ data ,contentId ,address}) => {
-      const navigate = useNavigate();
-      const handle = () => {
-      navigate(`/camping/${contentId}?keyword=${address}`);
-      }
-   
+const SearchedCampingCard = ({ data, contentId, address }) => {
+    const navigate = useNavigate();
+    const handle = () => {
+        navigate(`/camping/${contentId}?keyword=${address}`);
+    };
+
+    //     return (
+    //         <Card style={{ width: '35rem' }}>
+    //             <Card.Img variant="top" src={`${data?.firstImageUrl}`} style={{ height: '20rem' }} onClick={handle} />
+    //             <Card.Body style={{ height: '15rem' }}>
+    //                 <Card.Title>
+    //                     [{data?.doNm}&nbsp;
+    //                     {data?.sigunguNm}]&nbsp;{data?.facltNm}
+    //                 </Card.Title>
+    //                 <Card.Text className="camping-card-text">
+    //                     <p>{data?.lineIntro}</p>
+    //                     <p>{data?.intro}</p>
+    //                 </Card.Text>
+    //             </Card.Body>
+    //             <ListGroup className="list-group-flush">
+    //                 <ListGroup.Item>
+    //                     {data?.addr1}
+    //                     {data?.addr2}
+    //                 </ListGroup.Item>
+    //                 <ListGroup.Item>
+    //                     <Badge>{data?.resveCl}</Badge>&nbsp;&nbsp;&nbsp;
+    //                     <a href={`${data?.homepage}`}>{data?.homepage}</a>&nbsp;&nbsp;
+    //                     <span>{data?.tel}</span>
+    //                 </ListGroup.Item>
+    //                 <ListGroup.Item>
+    //                     {data?.sbrsCl.split(',').map((facility, index) => (
+    //                         <Badge key={index} bg="success" className=" justify-content-around py-2 me-1">
+    //                             {facility}
+    //                         </Badge>
+    //                     ))}
+    //                 </ListGroup.Item>
+    //             </ListGroup>
+    //         </Card>
+    //     );
+    // };
+
     return (
-        <Card style={{ width: '35rem' }}>
-            <Card.Img variant="top" src={`${data?.firstImageUrl}`} style={{ height: '20rem' }} onClick={handle}/>
-            <Card.Body style={{ height: '15rem' }}>
-                <Card.Title>
+        <Card className="justify-content-center camping-search-card mb-4">
+            <Card.Img
+                className="camping-card-img"
+                variant="top"
+                src={data?.firstImageUrl ? data.firstImageUrl : noImage}
+                onClick={handle}
+                alt={`${data?.facltNm}`}
+            />
+            <Card.Body>
+                <Card.Title className="camping-card-title">
                     [{data?.doNm}&nbsp;
                     {data?.sigunguNm}]&nbsp;{data?.facltNm}
                 </Card.Title>
@@ -29,17 +70,25 @@ const SearchedCampingCard = ({ data ,contentId ,address}) => {
             </Card.Body>
             <ListGroup className="list-group-flush">
                 <ListGroup.Item>
+                    <FontAwesomeIcon icon={faLocationDot} />
+                    &nbsp;
                     {data?.addr1}
                     {data?.addr2}
                 </ListGroup.Item>
                 <ListGroup.Item>
-                    <Badge>{data?.resveCl}</Badge>&nbsp;&nbsp;&nbsp;
-                    <a href={`${data?.homepage}`}>{data?.homepage}</a>&nbsp;&nbsp;
-                    <span>{data?.tel}</span>
+                    <Badge bg="warning">{data?.resveCl}</Badge>&nbsp;&nbsp;&nbsp;
+                    <p>
+                        <FontAwesomeIcon icon={faLink} /> &nbsp;
+                        <a href={`${data?.homepage}`}>{data?.homepage}</a>
+                    </p>
+                    <p>
+                        <FontAwesomeIcon icon={faPhone} /> &nbsp;
+                        {data?.tel}
+                    </p>
                 </ListGroup.Item>
                 <ListGroup.Item>
                     {data?.sbrsCl.split(',').map((facility, index) => (
-                        <Badge key={index} bg="success" className=" justify-content-around py-2 me-1">
+                        <Badge key={index} className=" justify-content-around me-1 info-badge">
                             {facility}
                         </Badge>
                     ))}
@@ -49,53 +98,4 @@ const SearchedCampingCard = ({ data ,contentId ,address}) => {
     );
 };
 
-   return (
-      <Card className="justify-content-center camping-search-card mb-4">
-         <Card.Img
-            className="camping-card-img"
-            variant="top"
-            src={data?.firstImageUrl ? data.firstImageUrl : noImage}
-            onClick={handle}
-            alt={`${data?.facltNm}`}
-         />
-         <Card.Body>
-            <Card.Title className="camping-card-title">
-               [{data?.doNm}&nbsp;
-               {data?.sigunguNm}]&nbsp;{data?.facltNm}
-            </Card.Title>
-            <Card.Text className="camping-card-text">
-               <p>{data?.lineIntro}</p>
-               <p>{data?.intro}</p>
-            </Card.Text>
-         </Card.Body>
-         <ListGroup className="list-group-flush">
-            <ListGroup.Item>
-               <FontAwesomeIcon icon={faLocationDot} />
-               &nbsp;
-               {data?.addr1}
-               {data?.addr2}
-            </ListGroup.Item>
-            <ListGroup.Item>
-               <Badge bg="warning">{data?.resveCl}</Badge>&nbsp;&nbsp;&nbsp;
-               <p>
-                  <FontAwesomeIcon icon={faLink} /> &nbsp;
-                  <a href={`${data?.homepage}`}>{data?.homepage}</a>
-               </p>
-               <p>
-                  <FontAwesomeIcon icon={faPhone} /> &nbsp;
-                  {data?.tel}
-               </p>
-            </ListGroup.Item>
-            <ListGroup.Item>
-               {data?.sbrsCl.split(',').map((facility, index) => (
-                  <Badge key={index} className=" justify-content-around me-1 info-badge">
-                     {facility}
-                  </Badge>
-               ))}
-            </ListGroup.Item>
-         </ListGroup>
-      </Card>
-   )
-}
-
-export default SearchedCampingCard
+export default SearchedCampingCard;
