350312ca1d3119a42ba962904ba88fb987b9f0f8
Molkang <mitier26@gmail.com>
2024-04-21 19:37:18 +0900

    console.log 삭제

--- src/components/TourCard/TourCard.jsx
+++ src/components/TourCard/TourCard.jsx
@@ -4,21 +4,20 @@ import { useNavigate } from 'react-router-dom';
 
 const TourCard = ({ data }) => {
     const navigate = useNavigate();
-    console.log(data)
-    let doName = data.addr1.split(" ");
+    let doName = data.addr1.split(' ');
     return (
         <div
             style={{
                 backgroundImage: 'url(' + data.firstimage + ')',
             }}
             className="tour-card"
-            onClick={()=>navigate(`/festival/${data.contentid}`)}
+            onClick={() => navigate(`/festival/${data.contentid}`)}
         >
             <div className="festival-slide-card">
-                <div className='festival-border'></div>
-                <div className='festival-text-bg'>
-                     <h2 className='festival-slide-card-text'>{data.title}</h2>
-                    <div className='festival-do'>{doName[0]}</div>
+                <div className="festival-border"></div>
+                <div className="festival-text-bg">
+                    <h2 className="festival-slide-card-text">{data.title}</h2>
+                    <div className="festival-do">{doName[0]}</div>
                     {/* <div>{data.addr2}</div> */}
                 </div>
             </div>
